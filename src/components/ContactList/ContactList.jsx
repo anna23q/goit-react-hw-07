5b5eebf77aa0d9b43f2a39d4aee26e46d9cccee6
anna23q <anblinda4@gmail.com>
2025-04-15 19:17:31 +0300

    addv index file

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,7 +3,7 @@ import Contact from '../Contact/Contact';
 import { useSelector } from 'react-redux';
 // import { selectContacts } from '../../redux/contactsSlice';
 // import { selectNameFilter } from '../../redux/filtersSlice';
-import { selectFilteredContacts } from '../../redux/contactsOps';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 // import { deleteContact } from '../../redux/contactsOps';
 
 export default function ContactList() {
